2817475b13eebe61a2df86b8ea6061529f4535d1
Will Delphia <willdelphia@gmail.com>
2019-06-20 22:44:31 -0500

    clean up and such

--- silly.js
+++ silly.js
@@ -1,193 +1,178 @@
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 function getRandomColor() {
-    return "rgb(" + getRandomInt(255) + "," + getRandomInt(255) + "," + getRandomInt(255) + ")";
+  return "rgb(" + getRandomInt(255) + "," + getRandomInt(255) + "," + getRandomInt(255) + ")";
 }
 
-  var silly = true;
-  var game = 0;
-  var start = null;
-  var stop = false;
-  var table =  document.getElementById('table');
-  var scoreDiv =  document.getElementById('score');
-  var highScoreDiv =  document.getElementById('high-score');
+var silly = true;
+var game = 0;
+var start = null;
+var stop = false;
+var table = document.getElementById('table');
+var scoreDiv = document.getElementById('score');
+var highScoreDiv = document.getElementById('high-score');
 
-  var body = document.getElementsByTagName("body")[0];
-  var sideLength = 300;
-  var top = 0;
-  var left = 0;
-  var right = sideLength - 20;
-  var pRightBound = sideLength - 50;
-  var bottom = sideLength - 20;
-  var paddleSensitivity = 10;
-  var highScore = 0;
+var body = document.getElementsByTagName("body")[0];
+var sideLength = 300;
+var top = 0;
+var left = 0;
+var right = sideLength - 20;
+var pRightBound = sideLength - 50;
+var bottom = sideLength - 20;
+var paddleSensitivity = 10;
+var highScore = 0;
 
 
-  initNewGame();
-  
-  function initNewGame() {
-    bouncedOnBottom = false;
-    deltaX = 0.6 * (game%2 ? 1 : -1);
-    deltaY = 1;
-    speed = 3;
-    x = getRandomInt(right) + 1;
-    y = 1;
-    points = 0;
-    pPos = 0;
-    pMove = null;
-    scoreDiv.innerHTML = points;
-    
-   
-    game++;
+initNewGame();
+
+function initNewGame() {
+  bouncedOnBottom = false;
+  deltaX = 0.6 * (game % 2 ? 1 : -1);
+  deltaY = 1;
+  speed = 3;
+  x = getRandomInt(right) + 1;
+  y = 1;
+  points = 0;
+  pPos = 0;
+  pMove = null;
+  scoreDiv.innerHTML = points;
+  game++;
+}
+
+function scoredAPoint() {
+  points++;
+  scoreDiv.innerHTML = points;
+
+  if (points > highScore) {
+      highScore = points;
+      highScoreDiv.innerHTML = highScore;
+  };
+  speed = speed + .5;
+}
+
+
+
+function renderFrame() {
+
+  var ctx = document.getElementById('table').getContext('2d');
+
+  if (!silly) {
+      ctx.clearRect(0, 0, 300, 300);
   }
-  
-  
-  function scoredAPoint() {
-    points++;
-    scoreDiv.innerHTML = points;
 
-    if (points > highScore){
-        highScore = points;
-        highScoreDiv.innerHTML = highScore;
-    };
-    speed = speed + .5;
+  ctx.fillStyle = 'rgb(0,0,0)';
+
+  var newPos = pPos;
+  if (pMove === "R") {
+      newPos = pPos + paddleSensitivity;
+  } else if (pMove === "L") {
+      newPos = pPos - paddleSensitivity;
   }
-  
-  
-  body.addEventListener('keydown', function(e){
-  
-  if(e.key==="ArrowRight"){
-     pMove = "R";
-  }
-  
-  if(e.key ==="ArrowLeft"){
-     pMove = "L";
-  }
-  });
-  
-  body.addEventListener('keyup', function(e){
-  
-  if(e.key==="ArrowRight"){
-     pMove = null;
-  }
-  
-  if(e.key ==="ArrowLeft"){
-     pMove = null;
-  }
-  });
-  
-  body.addEventListener('touchstart', function(e){
-    var centerX = window.innerWidth / 2;
-    var touchX = e.targetTouches[0].clientX;
 
-    if(touchX < centerX) {
-      pMove = "L";
-    }
-    else {
-      pMove = "R";
-    }
-  });
-
-
-  body.addEventListener('touchmove', function(e){
-    var centerX = window.innerWidth / 2;
-    var touchX = e.targetTouches[0].clientX;
-
-    if(touchX < centerX) {
-      pMove = "L";
-    }
-    else {
-      pMove = "R";
-    }
-  });
-
-  
-
-    
-  body.addEventListener('touchend', function(e){
-    pMove = null;
-  });
-
-
-  function step(timestamp) {
-    if (!start) start = timestamp;
-    
-    var ctx = document.getElementById('table').getContext('2d');
-
-    if(!silly){ 
-        ctx.clearRect(0, 0, 300, 300);
-    }
-
-    ctx.fillStyle = 'rgb(0,0,0)';
-   
-    var newPos = pPos;
-    if(pMove === "R") {
-         newPos = pPos + paddleSensitivity;
-        
-    }
-    else if (pMove === "L") {
-         newPos = pPos - paddleSensitivity;
-       
-    }
-    
-    if( newPos > pRightBound){
+  if (newPos > pRightBound) {
       newPos = pRightBound;
-    }
-  
-    if(newPos < 0) {
-      newPos =  0;
-    } 
-  
-    pPos = Math.floor(newPos);
-  
-    if(silly) {
-        ctx.fillStyle = getRandomColor();
-    }
-
-    ctx.fillRect(pPos, 295, 50, 5);
-  
-
-    x = Math.floor(x + (deltaX * speed));
-    y = Math.floor(y + (deltaY * speed));
-
-    var onPaddle =  x > (pPos - 20) &&  x < (pPos + 50);
-  
-    if(y > bottom  && onPaddle) {
-            if(deltaY > 0)  {
-                deltaY = deltaY * -1;
-            }
-        bouncedOnBottom = true;
-        scoredAPoint();
-    }
-    else if(y > bottom  && !onPaddle) {
-      initNewGame();
-    }
-  
-    if ( y < 0 ) {
-      if(deltaY < 0)  {
-        deltaY = deltaY * -1;
-      }
-    }
-    if( x < left) {
-      if(deltaX < 0) {
-        deltaX = deltaX * -1;
-      } 
-    }
-    if( x > right) {
-      if(deltaX > 0) {
-        deltaX = deltaX * -1;
-      } 
-    }
-       
-    if(silly) {
-        ctx.fillStyle = getRandomColor();
-    }
-    ctx.fillRect( x, y, 20, 20);
-
-    window.requestAnimationFrame(step);
-   
   }
-  
-  window.requestAnimationFrame(step);
+
+  if (newPos < 0) {
+      newPos = 0;
+  }
+
+  pPos = Math.floor(newPos);
+
+  if (silly) {
+      ctx.fillStyle = getRandomColor();
+  }
+
+  ctx.fillRect(pPos, 295, 50, 5);
+
+  x = Math.floor(x + (deltaX * speed));
+  y = Math.floor(y + (deltaY * speed));
+
+  var onPaddle = x > (pPos - 20) && x < (pPos + 50);
+
+  if (y > bottom && onPaddle) {
+      if (deltaY > 0) {
+          deltaY = deltaY * -1;
+      }
+      bouncedOnBottom = true;
+      scoredAPoint();
+  } else if (y > bottom && !onPaddle) {
+      initNewGame();
+  }
+
+  if (y < 0) {
+      if (deltaY < 0) {
+          deltaY = deltaY * -1;
+      }
+  }
+  if (x < left) {
+      if (deltaX < 0) {
+          deltaX = deltaX * -1;
+      }
+  }
+  if (x > right) {
+      if (deltaX > 0) {
+          deltaX = deltaX * -1;
+      }
+  }
+
+  if (silly) {
+      ctx.fillStyle = getRandomColor();
+  }
+  ctx.fillRect(x, y, 20, 20);
+
+  window.requestAnimationFrame(renderFrame);
+
+}
+
+window.requestAnimationFrame(renderFrame);
+
+
+
+
+/********* events **********/
+
+
+
+body.addEventListener('keydown', e => {
+
+  if (e.key === "ArrowRight") {
+      pMove = "R";
+  }
+
+  if (e.key === "ArrowLeft") {
+      pMove = "L";
+  }
+});
+
+body.addEventListener('keyup', e => {
+
+  if (e.key === "ArrowRight") {
+      pMove = null;
+  }
+
+  if (e.key === "ArrowLeft") {
+      pMove = null;
+  }
+});
+
+
+
+function touchHandler(e) {
+  var centerX = window.innerWidth / 2;
+  var touchX = e.targetTouches[0].clientX;
+
+  if (touchX < centerX) {
+      pMove = "L";
+  } else {
+      pMove = "R";
+  }
+}
+
+body.addEventListener('touchstart', touchHandler);
+body.addEventListener('touchmove', touchHandler);
+body.addEventListener('touchend', function(e) {
+  pMove = null;
+});
